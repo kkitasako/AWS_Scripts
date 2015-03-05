413e971fab9a8bb28ebad6b2b5ed6286d4165ef6
kkitasako <kkitasako@gmail.com>
2015-03-06 06:02:19 +0900

    Fix Cloudwatch metrics dimensions parameter

--- CloudWatch/cloudwatch_metrics_zabbix.sh
+++ CloudWatch/cloudwatch_metrics_zabbix.sh
@@ -11,13 +11,17 @@
 #-----------------------------------------------
 # ARGS & PARAMS
 #   $1 NAMESPACE
-#   $2 METRIC NAME
-#   $3 STATISTICS
+#   $2 DIMENSION NAME
+#   $3 DIMENSION VALUE
+#   $4 METRIC NAME
+#   $5 STATISTICS
 #-----------------------------------------------
 
 NAMESPACE=$1
-METRIC_NAME=$2
-STATISTICS=$3
+DIMENSION_NAME=$2
+DIMENSION_VALUE=$3
+METRIC_NAME=$4
+STATISTICS=$5
 
 # Set START_TIME as 1min before
 START_TIME=`date -u --date "2 minute ago" +"%FT%H:%M:00"`
@@ -33,5 +37,5 @@ REGION="ap-northeast-1"
 #  Get only statistics Value
 #-----------------------------------------------
 
-aws cloudwatch get-metric-statistics --region $REGION --namespace $NAMESPACE --metric-name $METRIC_NAME --start-time $START_TIME --end-time $END_TIME --period $PERIOD --statistics $STATISTICS | grep $STATISTICS | awk '{print $2}' | head -n 1 | sed -e "s/,//g"
+aws cloudwatch get-metric-statistics --region $REGION --namespace $NAMESPACE --dimensions Name=$DIMENSION_NAME,Value=$DIMENSION_VALUE --metric-name $METRIC_NAME --start-time $START_TIME --end-time $END_TIME --period $PERIOD --statistics $STATISTICS | grep $STATISTICS | awk '{print $2}' | head -n 1 | sed -e "s/,//g"
 
